742f265baf542c1cf2dc7b7e75079b7dd9d1e6e7
Harsh Garg <harshgargckt@gmail.com>
2023-12-18 23:14:37 +0530

    updating details

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 1. Install packages: `npm install`
 1. Edit the database configuration: `config/database.js`
 1. Create the database schema: `node scripts/create_database.js`
--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'Aditya@1304'
+        'password': 'harshgarg2003'
     },
 	'database': 'my_schema',
     'users_table': 'users'
